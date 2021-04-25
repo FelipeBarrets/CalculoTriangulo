eb17dad956d7f14f44ab360aa89c894b86442343
FelipeBarrets <souzafelipe1612@gmail.com>
2021-04-25 15:12:10 -0300

    versão 0.1

---
+++ CalculoTriangulo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CalculoTriangulo
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            double larg;
+            double alt;
+            Console.WriteLine("Insira a largura");
+            larg = double.Parse(Console.ReadLine());
+            Console.WriteLine("Insira a altura");
+            alt = double.Parse(Console.ReadLine());
+            area(larg, alt);
+            perimetro(larg, alt);
+            diametro(larg, alt);
+
+
+        }
+        static void area(double larg, double alt)
+        {
+            double area;
+            area = larg * alt;
+            Console.WriteLine("area= " + area.ToString("F"));
+        }
+        static void perimetro(double larg, double alt)
+        {
+            double perimetro;
+            perimetro = larg * 2 + alt * 2;
+            Console.WriteLine("perimetro= " + perimetro.ToString("F"));
+        }
+        static void diametro(double larg, double alt)
+        {
+            double diametro;
+            diametro = Math.Sqrt(larg * larg + alt * alt);
+            Console.WriteLine("diametro= " + diametro.ToString("F"));
+        }
+
+    }
+
+}
